14479e735e86584e149f0ebceac66c63be6d36f5
stefanieZhao77 <stefanieZhao77@users.noreply.github.com>
2025-04-06 19:36:41 +0000

    Deploying to gh-pages from @ stefanieZhao77/stefanieZhao77.github.io@99fb5ac8ef430c4ae145a84a432ef0b8b47ed620 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "post-no-planning-no-execution-how-to-plan-a-perfect-week",
+        },{id: "post-converting-a-step-file-to-mujoco-xml-for-physics-simulation-a-practical-guide",
+      
+        title: 'Converting a STEP File to MuJoCo XML for Physics Simulation: A Practical Guide... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.open("https://medium.com/@stefaniezhao414/converting-a-step-file-to-mujoco-xml-for-physics-simulation-a-practical-guide-e034ce617c55?source=rss-138ac7d61c06------2", "_blank");
+        
+      },
+    },{id: "post-no-planning-no-execution-how-to-plan-a-perfect-week",
       
         title: 'No planning, no execution — How to plan a perfect week <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
