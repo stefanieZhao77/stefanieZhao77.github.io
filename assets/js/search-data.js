f6f6d4c87ec207fcb3071d6bee22c5271debb59d
stefanieZhao77 <45490819+stefanieZhao77@users.noreply.github.com>
2026-03-18 09:14:52 +0000

    Deploying to gh-pages from @ stefanieZhao77/stefanieZhao77.github.io@649da45600947e05f0fd0845caf5f17f1eaf6589 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,6 +63,17 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
+          window.location.href = "/blog/2026/ai-geo-en/";
+        
+      },
+    },{id: "post-当-ai-成为入口-geo与控制权",
+      
+        title: "当 AI 成为入口：GEO与控制权",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
           window.location.href = "/blog/2026/agent-native-software-design/";
         
       },
