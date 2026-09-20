25cb6a503d6321ee351fb23faab401f6daa58a15
stefanieZhao77 <45490819+stefanieZhao77@users.noreply.github.com>
2026-09-20 13:21:39 +0000

    Deploying to gh-pages from @ stefanieZhao77/stefanieZhao77.github.io@1ffac38bebd49ae2f7bba2cf2dcb05f91e3cfdd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "post-what-is-jev-the-ai-model-that-stops-llms-writing-essays-for-yes-or-no-questions",
+        },{id: "post-最近很火的-jev-到底是什么-一篇讲清它干嘛用和怎么用",
+      
+        title: "最近很火的 Jev 到底是什么，一篇讲清它干嘛用和怎么用",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2026/jev-yu-yi-if/";
+        
+      },
+    },{id: "post-what-is-jev-the-ai-model-that-stops-llms-writing-essays-for-yes-or-no-questions",
       
         title: "What Is Jev? The AI Model That Stops LLMs Writing Essays for Yes-or-No...",
       
@@ -53,17 +64,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2026/what-is-jev-semantic-decision-model-en/";
-        
-      },
-    },{id: "post-最近很火的-jev-到底是什么-一篇讲清它干嘛用和怎么用",
-      
-        title: "最近很火的 Jev 到底是什么？一篇讲清它干嘛用和怎么用",
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2026/jev-yu-yi-if/";
         
       },
     },{id: "post-一千个-agent-自发站队-ai-也有羊群效应-而且比人更严重",
